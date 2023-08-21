cd6bee73ede893e8d38a5ad78356e12c392b89a3
Frame <gimenezzmaximiliano@gmail.com>
2023-08-21 18:56:42 -0300

    cambios al submit del form

--- src/01-useState/CounterHook.jsx
+++ src/01-useState/CounterHook.jsx
@@ -1,25 +1,10 @@
-import React, { useRef, useState, useTransition } from 'react'
+import React, { useState } from 'react'
 import { useFormik } from 'formik'
-
-
 
 export const CounterHook = () => {
     const [counter, setCounter] = useState(0)
-    const [value, setValue] = useState('')
     const [buttonValue, setButtonValue] = useState({buttonValueIncrement : `+${1}`,buttonValueDecrement : `-${1}`})
     const [userCustomValue, setCustomValue] = useState({customIncrement : 0,customDecrement: 0})
-    const setIncrement = () =>{ 
-        setValue('increment')
-    }
-    
-    const setDecrement = () =>{ 
-        setValue('decrement')
-    }
-
-    const setReset = () => {
-        setValue('reset')
-    }
-
     const handleIncrement = () => {
         if(counter + 1 > 1000 || counter + userCustomValue.customIncrement > 1000)return
         userCustomValue.customIncrement 
@@ -43,23 +28,23 @@ export const CounterHook = () => {
             name : ''
         },
         onSubmit: values => {
-            const { submitter } = event
-            if(submitter.name == 'incrementButton'){
+            const submitter = document.activeElement
+            if(submitter.name === 'incrementButton'){
                 if(values.customValue > 500)return
                 else if(values.customValue <= 0)return
-                else{
+                else
+                {
                     setCustomValue({...userCustomValue,customIncrement : parseInt(values.customValue)})
                     setButtonValue({...buttonValue,buttonValueIncrement : `+${values.customValue}`})
                 }
-                
             }
-            else if(submitter.name == 'decrementButton'){
+            else if(submitter.name === 'decrementButton'){
                 if(values.customValue > 500)return
                 else if(values.customValue <= 0)return
                 setCustomValue({...userCustomValue, customDecrement : parseInt(values.customValue)})
                 setButtonValue({...buttonValue,buttonValueDecrement : `-${values.customValue}`})
             }
-            else if(value === 'reset'){
+            else if(submitter.name === 'resetButton'){
                 setCustomValue({customDecrement:0,customIncrement:0})
                 setButtonValue({buttonValueIncrement:`+${1}`,buttonValueDecrement : `-${1}`})
             }
@@ -72,24 +57,19 @@ export const CounterHook = () => {
         <hr />
         <button className='btn btn-primary' onClick={handleIncrement} >{buttonValue.buttonValueIncrement}</button>
         <button className='btn btn-primary' onClick={handleReset} >reset</button>
-        <button className='btn btn-primary' onClick={handleDecrement}>{buttonValue.buttonValueDecrement}</button>
+        <button className='btn btn-primary' onClick={handleDecrement}>{buttonValue.buttonValueDecrement}
+        </button>
+        
         <form onSubmit={formik.handleSubmit}>
             <label className='h3 mb-3 mt-3' htmlFor="customValue">
                 CustomValue
             </label>
             <input className='form-control mb-3 bg-body-tertiary rounded shadow'type="text" id='customValue' name='customValue' onChange={formik.handleChange} value={formik.values.customValue}/>
-            <button type="submit" name='incrementButton' onClick={setIncrement} className='btn btn-primary ' >Apply value to increment</button>
-            <button className='btn btn-primary' type="submit" name='decrementButton' onClick={setDecrement} >Apply value to decrement</button>
-            <button className='btn btn-primary' type="submit" name='action' onClick={setReset} >reset Custom Values</button>
+            <button type="submit" name='incrementButton' className='btn btn-primary ' >Apply value to increment</button>
+            <button className='btn btn-primary' type="submit" name='decrementButton' >Apply value to decrement</button>
+            <button className='btn btn-primary' type="submit" name='resetButton' >reset Custom Values</button>
         </form>
     </div>
     )
 }
 
-/*
-    Explicacion del codigo :
-        Tengo un componente llamado CounterHook,
-        este tiene importado un hook que realiza lo siguiente :
-            toma un valor inicial, y se lo pasa a un useState
-            llamado counter. Este hook tambien tiene una funcion para sumar el counter. Luego se pasa el hook y se lo importa al CounterHook, y de ahi como este retorna un objeto, destructuramos el counter y la funcion plus. Por ultimo pasamos el plus al button +1 en el onClick
-*/
