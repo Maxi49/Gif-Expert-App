96214e7331375b787aab6edf7643f86f7fdba69d
Frame <gimenezzmaximiliano@gmail.com>
2023-08-21 14:15:17 -0300

    se elimino useCounter y se paso toda la logica a counterHook. Instale formik y hice un formulario a partir de este con un input adentro

--- src/01-useState/CounterHook.jsx
+++ src/01-useState/CounterHook.jsx
@@ -1,28 +1,79 @@
-import React, { useRef, useState } from 'react'
-import { useCounter } from '../hooks/useCounter'
-import useCustom from '../hooks/useCustom'
+import React, { useRef, useState, useTransition } from 'react'
+import { useFormik,Form } from 'formik'
+
 
 
 export const CounterHook = () => {
-    const customValue = useRef(0)
+    const [counter, setCounter] = useState(0)
+    const [value, setValue] = useState('')
 
-    const { counter,handleIncrement,handleReset,handleDecrement,initialValue,inputValue} = useCounter(1)
-    const {handleApplyDecrement,handleApplyIncrement,handleApplyReset } = useCustom(customValue,handleIncrement)
+    const setIncrement = () =>{ 
+        setValue('increment')
+    }
+    
+    const setDecrement = () =>{ 
+        setValue('decrement')
+    }
+
+    const setReset = () => {
+        setValue('reset')
+    }
+
+    const handleIncrement = () => {
+        userCustomValue.customIncrement 
+        ? setCounter(counter + userCustomValue.customIncrement)
+        : setCounter(counter + 1)
+    }
+    const handleDecrement = () => {
+        userCustomValue.customDecrement
+        ? setCounter(counter - userCustomValue.customDecrement)
+        : setCounter(counter - 1)
+    }
+    const handleReset = () => {
+        setCounter(0)
+    }
+
+    const [userCustomValue, setCustomValue] = useState(
+        {  
+            customIncrement : 0,
+            customDecrement: 0
+        }
+    )
+    const formik = useFormik({
+        initialValues: {
+          customValue: '',
+          action:''
+        },
+        onSubmit: values => {
+            if(value === 'increment'){
+                setCustomValue({...userCustomValue,customIncrement : parseInt(values.customValue)})
+            }
+            else if(value === 'decrement'){
+                setCustomValue({...userCustomValue, customDecrement : parseInt(values.customValue)})
+            }
+            else if(value === 'reset'){
+                setCustomValue({customDecrement:0,customIncrement:0})
+            }
+        },
+      });
     return (
     <>
         <h1>CounterHook: {counter} </h1>
         <hr />
         <button className='btn btn-primary' onClick={handleIncrement} >+1</button>
         <button className='btn btn-primary' onClick={handleReset} >reset</button>
-        <button className='btn btn-primary' onClick={()=> handleDecrement(2)}>-1</button>
-        <h2>Custom Value</h2>
-        <div className='input-group mb-3'>
-            <input type="text" ref={customValue}/>
-        </div>
-        <button className='btn btn-secondary' onClick={handleApplyIncrement} >Apply to Increment</button>
-        <button className='btn btn-secondary' onClick={handleApplyDecrement} >Apply to decrement</button>
-        <button className='btn btn-secondary' onClick={handleApplyReset} >Reset Values</button>
+        <button className='btn btn-primary' onClick={handleDecrement}>-1</button>
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="customValue">
+                CustomValue
+            </label>
+            <input type="text" id='customValue' name='customValue' onChange={formik.handleChange} value={formik.values.customValue}/>
+            <button type="submit" name='action' onClick={setIncrement} >Apply value to increment</button>
+            <button type="submit" name='action' onClick={setDecrement} >Apply value to decrement</button>
+            <button type="submit" name='action' onClick={setReset} >reset Custom Values</button>
+        </form>
     </>
+
     )
 }
 
